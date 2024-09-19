7341d2fb37c01b63a3d45ff6e663edef4f126351
Bruce Tuhakaraina <Bruce.Tuhakaraina@toiohomai.ac.nz>
2024-09-20 11:05:01 +1200

    Completed Project

---
+++ scripts/level.gd
@@ -0,0 +1,82 @@
+extends Node2D
+
+@export var next_level: PackedScene = null
+@export var is_final_level: bool = false
+
+@onready var start = $Start
+@onready var exit = $Exit
+@onready var death_zone = $Deathzone
+
+@onready var hud = $UILayer/HUD
+@onready var ui_layer = $UILayer
+
+var player = null
+
+@export var level_time = 5
+
+var timer_node = null
+var time_left
+
+var win = false
+
+func _ready():
+	player = get_tree().get_first_node_in_group("player")
+	if player!=null:
+		player.global_position = start.get_spawn_pos()
+	var traps = get_tree().get_nodes_in_group("traps")
+	for trap in traps:
+		#trap.connect("touched_player", _on_trap_touched_player)
+		trap.touched_player.connect(_on_trap_touched_player)
+	
+	exit.body_entered.connect(_on_exit_body_entered)
+	death_zone.body_entered.connect(_on_deathzone_body_entered)
+	
+	time_left = level_time
+	hud.set_time_label(time_left)
+	
+	timer_node = Timer.new()
+	timer_node.name = "Level Timer"
+	timer_node.wait_time = 1
+	timer_node.timeout.connect(_on_level_timer_timeout)
+	add_child(timer_node)
+	timer_node.start()
+
+func _on_level_timer_timeout():
+	if win == false:
+		time_left -= 1
+		hud.set_time_label(time_left)
+		if time_left < 0:
+			AudioPlayer.play_sfx("hurt")
+			reset_player()
+			time_left = level_time
+			hud.set_time_label(time_left)
+
+func _process(delta):
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+	elif Input.is_action_just_pressed("reset"):
+		get_tree().reload_current_scene()
+
+func _on_deathzone_body_entered(body):
+	AudioPlayer.play_sfx("hurt")
+	reset_player()
+
+func _on_trap_touched_player():
+	AudioPlayer.play_sfx("hurt")
+	reset_player()
+
+func reset_player():
+	player.velocity = Vector2.ZERO
+	player.global_position = start.get_spawn_pos()
+
+func _on_exit_body_entered(body):
+	if body is Player:
+		if is_final_level || (next_level != null):
+			exit.animate()
+			player.active = false
+			win = true
+			await get_tree().create_timer(1.5).timeout
+			if is_final_level:
+				ui_layer.show_win_screen(true)
+			else:
+				get_tree().change_scene_to_packed(next_level)
---
+++ scripts/player.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+class_name Player
+
+@export var gravity = 400
+@export var jump_force = 200
+@export var speed = 125
+
+@onready var animated_sprite = $AnimatedSprite2D
+
+var active = true
+
+func _physics_process(delta):
+	if is_on_floor()==false:
+		velocity.y += gravity * delta
+		if velocity.y > 500:
+			velocity.y = 500
+	
+	var direction = 0
+	if active==true:
+		if Input.is_action_just_pressed("jump"): #&& is_on_floor():
+			jump(jump_force)
+		
+		direction = Input.get_axis("move_left", "move_right")
+	if direction != 0:
+		animated_sprite.flip_h = (direction == -1)
+	
+	velocity.x = direction * speed
+	move_and_slide()
+	
+	update_animations(direction)
+
+func jump(force):
+	AudioPlayer.play_sfx("jump")
+	velocity.y = -force
+
+func update_animations(direction):
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite.play("idle")
+		else:
+			animated_sprite.play("run")
+	else:
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("fall")
+
+
